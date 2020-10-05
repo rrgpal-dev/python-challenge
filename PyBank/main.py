b2de38c571eace81c94353adb5136bb14b34b442
rrgpal-dev <71556924+rrgpal-dev@users.noreply.github.com>
2020-10-05 14:20:07 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,16 +6,20 @@ This is a temporary script file.
 """
 from itertools import islice
 num_of_months = 0
-
+total_amt = 0
+monthly_amt = [0.00]
+month = [""]
 with open ("budget_data.csv") as f:
   for i, x in enumerate(f):
      if i >=1 :
         num_of_months = i
-        
-        
-        
+        s = x.split(",")
+        month.append(s[0])
+        monthly_amt.append(s[1])
+
+    
 print("Financial Analysis")
 print("---------------------------------------------")
-print(f"Number of Monthsnum_of_months: {num_of_months}) 
+print(f"Total Months: {num_of_months}") 
         
         

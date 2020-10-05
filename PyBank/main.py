7cf72851a6e7a7baf426598b0bccb10d540cb479
rrgpal-dev <71556924+rrgpal-dev@users.noreply.github.com>
2020-10-05 13:31:24 -0400

    Number of Months pc of Code

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,5 +4,18 @@ Spyder Editor
 
 This is a temporary script file.
 """
+from itertools import islice
+num_of_months = 0
 
-print("hello")
+with open ("budget_data.csv") as f:
+  for i, x in enumerate(f):
+     if i >=1 :
+        num_of_months = i
+        
+        
+        
+print("Financial Analysis")
+print("---------------------------------------------")
+print(f"Number of Monthsnum_of_months: {num_of_months}) 
+        
+        

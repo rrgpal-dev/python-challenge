a33a3a745b5f0f7b9213dba8349ab46e203a5904
rrgpal-dev <71556924+rrgpal-dev@users.noreply.github.com>
2020-10-05 16:07:04 -0400

    checking in Total Votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,6 +3,20 @@
 """
 Created on Mon Oct  5 15:52:53 2020
 
-@author: N457149
+@author: N457149 - Raj Ramachandran
 """
-
+i = 0
+with open ("election_data.csv") as f:
+  for i, x in enumerate(f):
+     if i >=1 :
+        num_of_voters= i
+        s = x.split(",")
+        month.append(s[0])
+        monthly_amt.append(float(s[1]))
+        monthly_change.append(float(s[1]) - previous_mth_amt)
+        previous_mth_amt = float(s[1])
+ 
+    print("Election Results")
+    print("-----------------------------------")
+    print(f"Total Votes:  {num_of_votes}")
+    print("-----------------------------------")
